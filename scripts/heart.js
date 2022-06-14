158a1aeebb011c46788c680be2b047ac849c55ca
Darren Greaux <greauxd@gmail.com>
2022-06-14 13:57:15 -0400

    Edited css of heart image

--- scripts/heart.js
+++ scripts/heart.js
@@ -2,18 +2,18 @@
 function heartbeat() {
     let id = null;
     const elem = document.getElementById("heart");
-    let height = 5 + 'px';
-    let width = 5 + 'px'
+    let height = 20 + 'px';
+    let width = 20 + 'px'
     clearInterval(id);
     id = setInterval(size, 2);
 
     function size() {
-        if (height != 10 + 'px' && width != 10 + 'px') {
-            elem.style.height = (height + 5) + 'px';
-            elem.style.width = (width + 5) + 'px';
-
-        if (height == 10 + 'px' && width == 10 + 'px') {
+        while (height == 20 + 'px' && width == 20 + 'px') {
             elem.style.height = (height - 5) + 'px';
             elem.style.width = (width - 5) + 'px';
+
+        if (height == 15 + 'px' && width == 15 + 'px') {
+            elem.style.height = (height + 5) + 'px';
+            elem.style.width = (width + 5) + 'px';
     }
 }
