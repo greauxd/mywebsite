c5fbf644304a1dca4fac14f5ba9c1c9bfce53423
Darren Greaux <greauxd@gmail.com>
2022-07-08 12:24:11 -0400

    Cleaned up jasvascript for heart image and moved back to seperate file

--- scripts/heart.js
+++ scripts/heart.js
@@ -1,35 +1,28 @@
 // JavaScript source code
-function heartbeat() {
 
-    //Set id to null
-    let id = null;
+//Get element for image
 
-    //Get element for image
-    const elem = document.getElementById("heart");
+function startbeat() {
+	var elem = document.getElementById("heart");
 
-    //Default height and width
-    elem.style.height = "20px";
-    elem.style.width = "20px";
-    //Clear interval if any is present
-    clearInterval(id);
-    //Set interval delay
-    id = setInterval(function () {
-        shrink(elem);
-        grow(elem);
-    }, 2000);
+	//Set interval delay
+	setInterval(heartbeat(elem), 2000);
+}
 
-    function shrink(elem) {
-        //Changes height and width of image
-        if (elem.style.height == "20px" && elem.style.width == "20px") {
-            elem.style.height = "15px";
-            elem.style.width = "15px";
-        }
-    }
+
+function heartbeat(elem) {
+	//Changes height and width of image
+
+	if (elem.style.height == "20px" && elem.style.width == "20px") {
+		elem.style.height = "15px";
+		elem.style.width = "15px";
+	}
+
+
+	else {
+		elem.style.height = "20px";
+		elem.style.width = "20px";
+	}
 
 }
 
-function grow(elem) {
-    if (elem.style.height == "15px" && elem.style.width == "15px") {
-        elem.style.height = '20px';
-        elem.style.width = '20px';
-}
